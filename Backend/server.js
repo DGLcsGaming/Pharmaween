d9727761711b4fb13d48aed6fb36f741fd15b5cd
Fayçal Ghoul <dglcsgaming@gmail.com>
2019-05-25 20:45:15 +0100

    ejs

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var cors = require('cors');
 var bodyParser = require("body-parser");
-var path = require('path');
 var app = express();
+var path = require('path');
 var port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
@@ -15,16 +15,32 @@ var Routes = require('./Routes/Routes');
 
 app.use('/api', Routes);
 
-app.use(express.static(__dirname + '/files'));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/views'));
 
-app.get("/dashboard", function (req, res) {
-    res.status(200).sendFile(path.join(__dirname + '/files/dashboard.html'));
+// admin Views
+app.get("/admin", function (req, res) {
+    res.render('admin');
 });
+
+// Moderator Views
+app.get("/moderator", function (req, res) {
+    res.render('moderator');
+});
+app.get("/moderator-shifts", function (req, res) {
+    res.render('moderator-shifts');
+});
+app.get("/moderator-pharmacies", function (req, res) {
+    res.render('moderator-pharmacies');
+});
+
+// Login and Register Views
 app.get("/login", function (req, res) {
-    res.status(200).sendFile(path.join(__dirname + '/files/login.html'));
+    res.render('login');
 });
 app.get("/register", function (req, res) {
-    res.status(200).sendFile(path.join(__dirname + '/files/register.html'));
+    res.render('register');
 });
 
 app.listen(port, function () {
---
+++ Backend/views/js/register.js
@@ -0,0 +1,105 @@
+$(document).ready(function () {
+
+  ///////////////// Click and Submit Events //////////////////
+
+  $(document).on('click', '#logout', function () {
+    logout();
+  });
+
+  $(document).on('submit', '#formSignup', function (e) {
+    e.preventDefault();
+    e.stopImmediatePropagation();
+
+    var submitButton = $(this).find(':submit');
+    submitButton.prop('disabled', true);
+    $('#formContent-signup').css('filter', 'blur(50px)');
+    $('#loginLoading').show();
+
+    $.ajax({
+      url: '/api/register',
+      type: 'POST',
+      data: $('#formSignup').serialize(),
+      success: function (response) {
+        if (response.error == 0) {
+          alert(response.data);
+          window.location.replace('./login');
+        } else {
+          alert(JSON.stringify(response.data));
+        }
+      },
+      error: function (XMLHttpRequest, textStatus, errorThrown) {
+        alert("خطأ في الإتصال بالسيرفر, سنقوم بحل المشكلة قريبا, نعتذر على الإزعاج");
+      },
+      complete: function () {
+        submitButton.prop('disabled', false);
+        $('#formContent-signup').css('filter', 'blur(0px)');
+        $('#loginLoading').hide();
+      }
+    });
+    return false;
+  });
+  $("#formSignup").validate({
+    rules: {
+      first_name: {
+        required: true,
+        minlength: 2
+      },
+      last_name: {
+        required: true,
+        minlength: 2
+      },
+      email: {
+        required: true,
+        email: true
+      },
+      tel: {
+        required: true,
+        minlength: 10,
+        maxlength: 10,
+        number: true
+      },
+      password: {
+        required: true,
+        minlength: 8
+      },
+      confirm_password: {
+        required: true,
+        minlength: 8,
+        equalTo: "#password_signup"
+      }
+    },
+    messages: {
+      first_name: {
+        required: "الرجاء إدخال الإسم الأول",
+        minlength: "يجب على الإسم الأول أن يكون على الأقل مكون من حرفين"
+      },
+      last_name: {
+        required: "الرجاء إدخال إسم العائلة",
+        minlength: "يجب على إسم العائلة أن يكون على الأقل مكون من حرفين"
+      },
+      email: {
+        required: "الرجاء إدخال البريد إلكتروني",
+        email: "الرجاء إدخال بريد إلكتروني صحيح"
+      },
+      tel: {
+        required: "الرجاء إدخال رقم الهاتف",
+        minlength: "يجب على رقم الهاتف أن يتكون على من 10 أرقام",
+        maxlength: "يجب على رقم الهاتف أن يتكون على من 10 أرقام",
+        number: "الرجاء إدخال أرقام فقط"
+      },
+      password: {
+        required: "الرجاء إدخال كلمة سر",
+        minlength: "يجب على كلمة السر أن تتكون على الأقل من 8 حروف"
+      },
+      confirm_password: {
+        required: "الرجاء إعادة إدخال كلمة السر",
+        minlength: "يجب على كلمة السر أن تتكون على الأقل من 8 حروف",
+        equalTo: "كلمتا السر غير متطابقتان"
+      }
+    },
+    errorClass: "is-invalid",
+    validClass: "is-valid"
+  });
+  ///////////////////////////////////////////////
+
+});
